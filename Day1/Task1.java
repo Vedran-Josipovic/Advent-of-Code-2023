ea1bd5fb55837ec829d5e682430bf84431c0dd95
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2023-12-01 08:37:11 +0100

    Day-1: Task-1 [Completed]

--- Day1/Task1.java
+++ Day1/Task1.java
@@ -1,7 +1,29 @@
 package Day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("1.1");
+        File data = new File("Day1/data");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(data);
+        } catch (FileNotFoundException e) {
+            System.exit(-1);
+        }
+        Integer sum = 0;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            List<Integer> characters = line
+                    .chars()
+                    .filter(i -> i >= 48 && i <= 57)
+                    .map(i -> i - 48)
+                    .boxed().toList();
+            sum += (characters.getFirst() * 10) + characters.getLast();
+        }
+        System.out.println(sum);
     }
 }
